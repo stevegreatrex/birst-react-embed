1cba4d0acbd53014f939f3352b6805e76e2fc4b8
Steve Greatrex <stephen.greatrex@googlemail.com>
2017-09-19 11:50:27 +0100

    BirstComponent tests
    Tests for component lifetime & rendering

--- src/BirstComponent.spec.tsx
+++ src/BirstComponent.spec.tsx
@@ -1,19 +1,85 @@
 import * as React from 'react';
 import { IBqlQuery } from './birst/IBqlQuery';
-import { connectToBirst, LoadingPlaceholder } from './BirstComponent';
+import { IBqlResults } from './birst/IBqlResults';
+import { connectToBirst, LoadingPlaceholder, ErrorPlaceholder } from './BirstComponent';
+import { instance as bqlApi } from './birst/BqlApi';
 import { shallow, mount, render } from 'enzyme';
+import { ManualPromise } from './ManualPromise';
+import { waitForNextFrame } from './delay';
 
-jest.mock('./birst/BqlApi');
+const executeQuery = jest.spyOn(bqlApi, 'executeQuery');
 
 describe('connectToBirst', () => {
+	type PropsFromQuery = {
+		fromQuery?: string;
+	};
+
+	type CombinedProps = PropsFromQuery & {
+		fromProps?: string;
+	};
+
+	const Component = (props: CombinedProps) => (
+		<div>
+			<span>{props.fromQuery}</span>
+			<span>{props.fromProps}</span>
+		</div>
+	);
+	const query: IBqlQuery<PropsFromQuery> = {
+		bql: 'bql command',
+		mapResultsToProps: results => ({ fromQuery: 'not used due to mocking' })
+	};
+
+	const ConnectedComponent = connectToBirst(query)(Component);
+
 	it('should initially render LoadingComponent', () => {
-		const Component = () => <div>inner component</div>;
-		const query: IBqlQuery<{}> = {
-			bql: 'bql command',
-			mapResultsToProps: results => ({})
-		};
-		const ConnectedComponent = connectToBirst(query)(Component);
 		const wrapper = shallow(<ConnectedComponent />);
-		expect(wrapper.contains(<LoadingPlaceholder />)).toBe(true);
+
+		expect(wrapper.find(LoadingPlaceholder).exists()).toBe(true);
+		expect(wrapper.find(Component).exists()).toBe(false);
+	});
+
+	it('should render inner component after load', async done => {
+		const apiResponse = new ManualPromise<PropsFromQuery>();
+		executeQuery.mockReturnValue(apiResponse);
+
+		const wrapper = mount(<ConnectedComponent />);
+
+		expect(executeQuery).toBeCalledWith(query);
+		expect(wrapper.find(LoadingPlaceholder).exists()).toBe(true);
+		expect(wrapper.find(Component).exists()).toBe(false);
+
+		apiResponse.resolve({ fromQuery: 'results' });
+		await waitForNextFrame();
+
+		expect(wrapper.find(LoadingPlaceholder).exists()).toBe(false);
+		expect(wrapper.find(Component).exists()).toBe(true);
+		done();
+	});
+
+	it('should start combine props from query & parent props', async done => {
+		const apiResponse = Promise.resolve({ fromQuery: 'results' });
+		executeQuery.mockReturnValue(apiResponse);
+
+		const wrapper = mount(<ConnectedComponent fromProps="props" />);
+		await waitForNextFrame();
+
+		expect(wrapper.find(Component).props().fromQuery).toEqual('results');
+		expect(wrapper.find(Component).props().fromProps).toEqual('props');
+		done();
+	});
+
+	it('should render error details', async done => {
+		const apiResponse = new ManualPromise<PropsFromQuery>();
+		executeQuery.mockReturnValue(apiResponse);
+
+		const wrapper = mount(<ConnectedComponent />);
+
+		apiResponse.reject('oh no!');
+		await waitForNextFrame();
+
+		expect(wrapper.find(LoadingPlaceholder).exists()).toBe(false);
+		expect(wrapper.find(ErrorPlaceholder).exists()).toBe(true);
+		expect(wrapper.find(ErrorPlaceholder).props().error).toEqual('oh no!');
+		done();
 	});
 });
